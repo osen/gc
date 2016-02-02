46613cbf0b82154dc57ea9aa0393d0b0363f6871
osen <kpedersen@live.co.uk>
2016-02-02 21:26:37 +0000

    Include guards.

--- include/gc.h
+++ include/gc.h
@@ -1,3 +1,6 @@
+#ifndef _GC_H
+#define _GC_H
+
 #include <string.h>
 
 struct GcContext;
@@ -7,3 +10,5 @@ void *gc_root(struct GcContext *ctx, size_t size);
 void *gc_alloc(struct GcContext *ctx, size_t size);
 int gc_finalizer(struct GcContext *ctx, void *ptr, void (*deleter)(void*));
 void gc_collect(struct GcContext *ctx);
+
+#endif
